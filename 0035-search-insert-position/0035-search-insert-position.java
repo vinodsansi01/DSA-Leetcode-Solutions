0a18b8c6b607dc11dcbb8f6b4a46a7272579f8fe
Vinod Sansi <vinod.sansi01@gmail.com>
2025-12-20 12:22:19 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31.15%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        int start = 0, end = nums.length-1, ans = nums.length;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+                if(nums[mid] == target){ 
+                    return mid;
+                }
+                 else if(nums[mid] < target){
+                    start = mid +1;
+
+                }else{
+                    ans = mid;
+                    end = mid-1;
+
+                }
+
+        }
+        return ans;
+        
+    }
+}
